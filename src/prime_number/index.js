1348cf75d16ad31a3e2868edb5faace66f89c30b
kolyjjj <me.koly.li@gmail.com>
2016-02-03 14:32:09 +0800

    implement the prime number functionality

--- src/prime_number/index.js
+++ src/prime_number/index.js
@@ -1,5 +1,18 @@
 const primeNumber = function(start, end) {
-  return null;
+  if (start < 0 || end <= 0 || start > end) return null;
+  let result = [];
+  for (let i = start; i <= end; i++) {
+    if (isPrimeNumber(i)) result.push(i);
+  }
+  return result.length === 0 ? null : result;
 };
+
+export function isPrimeNumber(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i*i <= num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
 
 export default primeNumber;
--- test/prime_number/index.spec.js
+++ test/prime_number/index.spec.js
@@ -8,4 +8,49 @@ describe('prime number', ()=>{
     assert.equal(primeNumber(14, 15), null); 
   });
 
-})
+  it('should return an array if there is prime number in the range', ()=>{
+    assert.deepEqual(primeNumber(2, 3), [2,3]);
+  });
+
+  it('should return [2,3] given start 2 end 4', ()=>{
+    assert.deepEqual(primeNumber(2,4), [2,3]);
+  });
+
+  it('should return [2,3] start from 0', ()=>{
+    assert.deepEqual(primeNumber(0, 4), [2,3]);
+  });
+
+  it('should return an array given the same number as range', ()=>{
+    assert.deepEqual(primeNumber(2, 2), [2]);
+  });
+
+  it('should return an array given special 1 as start', ()=>{
+    assert.deepEqual(primeNumber(1, 2), [2]);
+  })
+
+  it('should return an array given a larger range', ()=>{
+    assert.deepEqual(primeNumber(347, 359), [347, 349, 353, 359]);
+  });
+
+  it('here is a big one start 0 end 1000', ()=>{
+    assert.deepEqual(primeNumber(0, 1000), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]);
+  });
+
+  describe('invalid input', ()=>{
+    it('should return null given 0 and 1 as range', ()=>{
+      assert.equal(primeNumber(0, 1), null);
+    });
+
+    it('should return null given minus number as range', ()=>{
+      assert.equal(primeNumber(-1, 0), null);
+      assert.equal(primeNumber(0, -1), null);
+      assert.equal(primeNumber(-1, 2), null);
+      assert.equal(primeNumber(2, -1), null);
+    })
+
+    it('should return null given start lower than range', ()=>{
+      assert.equal(primeNumber(13, 10), null);
+    })
+  });
+});
+
